a6d438f8006c1f43fc5405bffec19c886e3e2401
Filippo Cremonese <filippocremonese@rev.ng>
2020-08-31 12:14:38 +0200

    Refactoring to improve readability

--- orchestra/model/actions/install.py
+++ orchestra/model/actions/install.py
@@ -14,10 +14,10 @@ class InstallAction(Action):
     def __init__(self, build, script, index):
         super().__init__("install", build, script, index)
 
-    def _run(self, show_output=False):
-        genv = global_env(self.index.config)
-        tmp_root = genv["TMP_ROOT"]
-        orchestra_root = genv['ORCHESTRA_ROOT']
+    def _run(self, show_output=False, perform_merge=True, create_binary_archive=False):
+        environment = global_env(self.index.config)
+        tmp_root = environment["TMP_ROOT"]
+        orchestra_root = environment['ORCHESTRA_ROOT']
         rpath_placeholder = self.index.config["options"]["rpath_placeholder"]
 
         self._prepare_tmproot()
@@ -29,44 +29,17 @@ class InstallAction(Action):
         new_files = [f for f in post_file_list if f not in pre_file_list]
 
         # TODO: uninstall the currently installed build of this component if there's one
-        # TODO: do all the stuff that install-in-temp did (see string below)
+        self._uninstall_currently_installed_build()
 
-        hard_to_symbolic = """hard-to-symbolic.py "${TMP_ROOT}${ORCHESTRA_ROOT}" """
-        run_script(hard_to_symbolic, show_output=show_output, environment=self.environment)
+        self._hard_to_symbolic(show_output)
 
-        fix_rpath_script = dedent(f"""
-        cd "$TMP_ROOT$ORCHESTRA_ROOT"
-        # Fix rpath
-        find . -type f -executable | while read EXECUTABLE; do
-          if head -c 4 "$EXECUTABLE" | grep '^.ELF' > /dev/null && 
-                file "$EXECUTABLE" | grep x86-64 | grep -E '(shared|dynamic)' > /dev/null; then
-            REPLACE='$'ORIGIN/$(realpath --relative-to="$(dirname "$EXECUTABLE")" ".")
-            echo "Setting rpath to $REPLACE"
-            elf-replace-dynstr.py "$EXECUTABLE" "{rpath_placeholder}" "$REPLACE"
-            elf-replace-dynstr.py "$EXECUTABLE" "$ORCHESTRA_ROOT" "$REPLACE"
-          fi
-        done
-        """)
-        run_script(fix_rpath_script, show_output=show_output, environment=self.environment)
+        self._fix_rpath(rpath_placeholder, show_output)
 
-        ndebug = "0"
-        debug = "1" if ndebug == "0" else "0"
-        patch_ndebug_script = dedent(rf"""
-        cd "$TMP_ROOT$ORCHESTRA_ROOT"
-        find include/ -name "*.h" \
-          -exec \
-            sed -i \
-            -e 's|^\s*#\s*ifndef\s\+NDEBUG|#if {debug}|' \
-            -e 's|^\s*#\s*ifdef\s\+NDEBUG|#if {ndebug}|' \
-            -e 's|^\(\s*#\s*if\s\+.*\)!defined(NDEBUG)|\1{debug}|' \
-            -e 's|^\(\s*#\s*if\s\+.*\)defined(NDEBUG)|\1{ndebug}|' \
-            {"{}"} ';'
-        """)
-        run_script(patch_ndebug_script, show_output=show_output, environment=self.environment)
+        self._replace_ndebug(False, show_output)
 
-        copy_command = f'cp -farl "{tmp_root}/{orchestra_root}/." "{orchestra_root}"'
-        run_script(copy_command, show_output=show_output, environment=self.environment)
+        self._merge(show_output)
 
+        # Write file index
         os.makedirs(install_component_dir(self.index.config), exist_ok=True)
         installed_component_path = install_component_path(self.build.component.name, self.index.config)
         with open(installed_component_path, "w") as f:
@@ -109,6 +82,50 @@ class InstallAction(Action):
         ]
         for p in paths_to_create:
             os.makedirs(f"{tmp_root}/{orchestra_root}/{p}", exist_ok=True)
+
+    def _hard_to_symbolic(self, show_output):
+        hard_to_symbolic = """hard-to-symbolic.py "${TMP_ROOT}${ORCHESTRA_ROOT}" """
+        run_script(hard_to_symbolic, show_output=show_output, environment=self.environment)
+
+    def _fix_rpath(self, rpath_placeholder, show_output):
+        fix_rpath_script = dedent(f"""
+            cd "$TMP_ROOT$ORCHESTRA_ROOT"
+            # Fix rpath
+            find . -type f -executable | while read EXECUTABLE; do
+                if head -c 4 "$EXECUTABLE" | grep '^.ELF' > /dev/null &&
+                        file "$EXECUTABLE" | grep x86-64 | grep -E '(shared|dynamic)' > /dev/null;
+                then
+                    REPLACE='$'ORIGIN/$(realpath --relative-to="$(dirname "$EXECUTABLE")" ".")
+                    echo "Setting rpath to $REPLACE"
+                    elf-replace-dynstr.py "$EXECUTABLE" "{rpath_placeholder}" "$REPLACE"
+                    elf-replace-dynstr.py "$EXECUTABLE" "$ORCHESTRA_ROOT" "$REPLACE"
+                fi
+            done
+            """)
+        run_script(fix_rpath_script, show_output=show_output, environment=self.environment)
+
+    def _replace_ndebug(self, enable_debugging, show_output):
+        debug, ndebug = ("1", "0") if enable_debugging else ("0", "1")
+        patch_ndebug_script = dedent(rf"""
+                cd "$TMP_ROOT$ORCHESTRA_ROOT"
+                find include/ -name "*.h" \
+                  -exec \
+                    sed -i \
+                    -e 's|^\s*#\s*ifndef\s\+NDEBUG|#if {debug}|' \
+                    -e 's|^\s*#\s*ifdef\s\+NDEBUG|#if {ndebug}|' \
+                    -e 's|^\(\s*#\s*if\s\+.*\)!defined(NDEBUG)|\1{debug}|' \
+                    -e 's|^\(\s*#\s*if\s\+.*\)defined(NDEBUG)|\1{ndebug}|' \
+                    {"{}"} ';'
+                """)
+        run_script(patch_ndebug_script, show_output=show_output, environment=self.environment)
+
+    def _merge(self, show_output):
+        environment = global_env(self.index.config)
+        tmp_root = environment["TMP_ROOT"]
+        orchestra_root = environment['ORCHESTRA_ROOT']
+
+        copy_command = f'cp -farl "{tmp_root}/{orchestra_root}/." "{orchestra_root}"'
+        run_script(copy_command, show_output=show_output, environment=self.environment)
 
     @staticmethod
     def _index_directory(dirpath, strip_prefix=None):
