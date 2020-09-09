520104b228e08b1341446c5aa850c1f61cd8d20d
Filippo Cremonese <filippocremonese@rev.ng>
2020-09-09 19:04:29 +0200

    Added short version of --force

--- orchestra/cmds/configure.py
+++ orchestra/cmds/configure.py
@@ -5,7 +5,7 @@ from ..executor import Executor
 def install_subcommand(sub_argparser):
     cmd_parser = sub_argparser.add_parser("configure", handler=handle_configure)
     cmd_parser.add_argument("component")
-    cmd_parser.add_argument("--force", action="store_true", help="Force execution of the root action")
+    cmd_parser.add_argument("--force", "-f", action="store_true", help="Force execution of the root action")
 
 
 def handle_configure(args, config: Configuration):
--- orchestra/cmds/install.py
+++ orchestra/cmds/install.py
@@ -5,7 +5,7 @@ from ..executor import Executor
 def install_subcommand(sub_argparser):
     cmd_parser = sub_argparser.add_parser("install", handler=handle_install)
     cmd_parser.add_argument("component")
-    cmd_parser.add_argument("--force", action="store_true", help="Force execution of the root action")
+    cmd_parser.add_argument("--force", "-f", action="store_true", help="Force execution of the root action")
     cmd_parser.add_argument("--no-merge", action="store_true", help="Do not merge files into Orchestra root")
     cmd_parser.add_argument("--create-binary-archives", action="store_true", help="Create binary archives")
 
