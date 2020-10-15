8facfdde69c6f76dae16bf68792d70babdba6297
Filippo Cremonese <filippocremonese@rev.ng>
2020-10-15 15:26:21 +0200

    More robust interrupted configure handling

--- orchestra/actions/configure.py
+++ orchestra/actions/configure.py
@@ -14,8 +14,12 @@ class ConfigureAction(Action):
         return os.path.exists(self._configure_successful_path)
 
     def _run(self, args):
-        if os.path.exists(self.environment["BUILD_DIR"]):
-            logger.warning("Build directory exists, running configure script anyway")
+        if os.path.exists(self._configure_successful_path):
+            logger.warning("This component was already successfully configured, rerunning configure script")
+            os.remove(self._configure_successful_path)
+        elif os.path.exists(self.environment["BUILD_DIR"]):
+            logger.warning("Previous configure probably failed, running configure script in a dirty environment")
+            logger.warning(f"You might want to delete the build directory (use `orchestra clean`)")
 
         result = run_script(self.script, quiet=args.quiet, environment=self.environment)
         if result.returncode == 0:
