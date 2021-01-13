f01cc96edcacd6f9967905c77c74140768462067
Filippo Cremonese <filippocremonese@rev.ng>
2021-01-13 11:49:25 +0100

    Fix help message indentation

--- orchestra/cmds/update.py
+++ orchestra/cmds/update.py
@@ -78,8 +78,9 @@ def handle_update(args):
                 failed_pulls.append(f"Repository {component.name}")
 
     if failed_pulls:
-        formatted_failed_pulls = "\n".join([f"  {repo}" for repo in failed_pulls])
-        failed_git_pull_suggestion = dedent(f"""
+        formatted_failed_pulls = "\n".join([f"  - {repo}" for repo in failed_pulls])
+        # Note: f-strings don't account for indentation, using a template is more practical
+        failed_git_pull_template = dedent("""
         Could not git pull --ff-only the following repositories:
         {formatted_failed_pulls}
 
@@ -89,11 +90,13 @@ def handle_update(args):
             - `git pull --rebase`, to pull remote changes and apply your commits on top
             - `git push` your changes to the remotes
         """)
+        failed_git_pull_suggestion = failed_git_pull_template.format(formatted_failed_pulls=formatted_failed_pulls)
         logger.error(failed_git_pull_suggestion)
 
     if failed_clones:
-        formatted_failed_clones = "\n".join([f"  {repo}" for repo in failed_clones])
-        failed_git_clone_suggestion = dedent(f"""
+        formatted_failed_clones = "\n".join([f"  - {repo}" for repo in failed_clones])
+        # Note: f-strings don't account for indentation, using a template is more practical
+        failed_git_clone_template = dedent("""
                 Could not clone the following repositories:
                 {formatted_failed_clones}
 
@@ -101,6 +104,7 @@ def handle_update(args):
                     - check your network connection
                     - check your ssh and git configuration (try manually cloning the repositories)
                 """)
+        failed_git_clone_suggestion = failed_git_clone_template.format(formatted_failed_clones=formatted_failed_clones)
         logger.error(failed_git_clone_suggestion)
 
     if failed_pulls or failed_clones:
